29b889850708935ab719bd4b1c4fa2dad459f506
Joel <joelbarnett2000@gmail.com>
2025-01-17 16:16:29 +0000

    test

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "Practice-bucket-jan25" {
-  bucket = "Practice-bucket-jan25"
+resource "aws_s3_bucket" "practice-bucket-jan25" {
+  bucket = "practice-bucket-jan25"
   }
 
 
