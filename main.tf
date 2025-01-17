46337deb254e9b1244ab7927fdc9f1ac2d6ffa17
Joel <joelbarnett2000@gmail.com>
2025-01-17 16:06:33 +0000

    new bucket

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraform-nov-project24" {
-  bucket = "terraform-nov-project24"
+resource "aws_s3_bucket" "Practice-bucket-JB" {
+  bucket = "Practice-bucket-JB"
   }
+
