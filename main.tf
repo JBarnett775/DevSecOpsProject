a1a4cae0265bd06d53d65f3a6b29bac9fc91f403
Joel <joelbarnett2000@gmail.com>
2025-01-19 16:31:10 +0000

    testingnewbucket

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "practice-bucket-jan25" {
+  bucket = "practice-bucket-jan25"
+  }
 resource "aws_s3_bucket" "testing_code_worked" {
   bucket = "testing_code_worked"
   }
   terraform { 
     backend s3 {
     # Replace this with your bucket name!
-    bucket         = "testing_code_worked"
+    bucket         = "practice-bucket-jan25"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-west-2"
   }
