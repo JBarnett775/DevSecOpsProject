d0878f2e8a3ec4a21d0d9ab33fe4221e7f888a31
Joel <joelbarnett2000@gmail.com>
2025-01-19 15:46:22 +0000

    updateb4GH

--- main.tf
+++ main.tf
@@ -4,5 +4,13 @@ provider "aws" {
 resource "aws_s3_bucket" "practice-bucket-jan25" {
   bucket = "practice-bucket-jan25"
   }
+  terraform { 
+    backend s3 {
+    # Replace this with your bucket name!
+    bucket         = "practice-bucket-jan25"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
 
 
