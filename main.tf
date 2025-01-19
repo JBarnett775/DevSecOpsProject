75d20e0160546124352c5f850961ae798c55b842
Joel <joelbarnett2000@gmail.com>
2025-01-19 18:03:13 +0000

    pb

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "practice-bucket-jan25" {
-  bucket = "practice-bucket-jan25"
+resource "aws_s3_bucket" "projectbucket" {
+  bucket = "projectbucket"
   }
   terraform { 
     backend s3 {
